83f183c8fcf311705b3493ff899e0ed0c8164e3e
Crowdbotics <team@crowdbotics.com>
2024-02-09 14:27:10 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "women-in-identity-46177",
+  url: "https://women-in-identity-46177.botics.co",
+  api: "https://women-in-identity-46177.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
